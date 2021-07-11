bd2202c121b78c15db46f41a6c7a613e872c3823
Henri Thomas <hthoma@henri-server.i.fungalgenomics.ca>
2021-07-11 23:15:21 +0000

    new assembly forms

--- smithy/assembly/urls.py
+++ smithy/assembly/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='assembly-home'),
-    path('about/', views.about, name='assembly-about')
+    path('about/', views.about, name='assembly-about'),
+    path('gibson/', views.gibson_form, name='gibson-form'),
+    path('goldengate/', views.goldengate_form, name='goldengate-form')
 ]
--- smithy/assembly/views.py
+++ smithy/assembly/views.py
@@ -5,3 +5,9 @@ def home(request):
 
 def about(request):
     return render(request, 'assembly/about.html', {'title': 'Assembly About'}) 
+
+def gibson_form(request):
+    return render(request, 'assembly/gibson_form.html', {'title': 'Gibson Assembly'})
+    
+def goldengate_form(request):
+    return render(request, 'assembly/goldengate_form.html', {'title': 'Golden Gate Assembly'})
