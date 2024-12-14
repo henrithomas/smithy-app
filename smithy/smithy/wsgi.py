9e63388ec653e9b25f580f3c36f169d66cca76f7
Henri Thomas <hmgdthomas@gmail.com>
2024-12-14 23:52:01 +0000

    updated wsgi for gnunicorn and project directory

--- smithy/smithy/wsgi.py
+++ smithy/smithy/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.insert(0, os.path.join(Path(__file__).resolve().parent.parent.parent))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smithy.settings')
 
 application = get_wsgi_application()
