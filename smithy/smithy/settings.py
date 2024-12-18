1c17eed6b4aa77768759d2f66831b0eeb3c95691
Henri Thomas <hmgdthomas@gmail.com>
2024-12-18 06:02:53 +0000

    secret key updated, links temporarily updated for examples

--- smithy/smithy/settings.py
+++ smithy/smithy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&k^-yh_m+%g%$oyp4i43%ds0koe6fnfj_$wuoq1t2nv3!x90f'
+SECRET_KEY = os.environ['SMITHY_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
